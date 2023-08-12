d2e3579f8a04515fefcb742d2520bd69f1c8906b
NurulAfsar96 <nurulafsar.jony96@gmail.com>
2023-08-12 13:28:24 +0600

    Report And Snapshot generating

--- src/test/java/afsar/Pages/LoginPage.java
+++ src/test/java/afsar/Pages/LoginPage.java
@@ -1,18 +1,26 @@
 package afsar.pages;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 
 import afsar.drivers.PageDriver;
 import afsar.utilities.CommonMethods;
+import afsar.utilities.GetScreenshot;
 
 public class LoginPage extends CommonMethods {
-
-	public LoginPage()
+	ExtentTest test;
+	public LoginPage(ExtentTest test)
 	{
 		PageFactory.initElements(PageDriver.getCurrentDriver(), this);
+		this.test = test;
 	}
 	@FindBys({
 		@FindBy(name="username"),
@@ -33,7 +41,7 @@ public class LoginPage extends CommonMethods {
 	})
 	WebElement loginBtn;
 	
-	public void login() throws InterruptedException {
+	public void login() throws InterruptedException, IOException {
 		timeout();
 		try {
 			if(uname.isDisplayed()) { 
@@ -43,7 +51,15 @@ public class LoginPage extends CommonMethods {
 			
 		} catch (Exception e)
 		{
-			System.out.println(e);
+			test.fail("<p style=\"color:#FF5353; font-size:13px\"><b>Username is not locateable.Please check the error message.</b></p>");
+			Throwable t = new InterruptedException("Exception");
+			test.fail(t);
+			@SuppressWarnings("unused")
+			String screenShotPath = GetScreenshot.capture(PageDriver.getCurrentDriver(), "usernameLocator");
+			String dest = System.getProperty("user.dir") + "\\screenshots\\" + "usernameLocator.png";
+			test.fail(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
+			Assert.assertTrue(uname.isDisplayed());
+			PageDriver.getCurrentDriver().quit();
 		}
 	
 		try {
@@ -55,19 +71,39 @@ public class LoginPage extends CommonMethods {
 		}
 		catch (Exception e)
 		{
-			System.out.println(e);
+			test.fail("<p style=\"color:#FF5353; font-size:13px\"><b>Password is not locateable.Please check the error message.</b></p>");
+			Throwable t = new InterruptedException("Exception");
+			test.fail(t);
+			@SuppressWarnings("unused")
+			String screenShotPath = GetScreenshot.capture(PageDriver.getCurrentDriver(), "passwordLocator");
+			String dest = System.getProperty("user.dir") + "\\screenshots\\" + "passwordLocator.png";
+			test.fail(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
+			Assert.assertTrue(passwd.isDisplayed());
+			PageDriver.getCurrentDriver().quit();
 		}
 			
 		try {
 			if(loginBtn.isDisplayed()) { 
 				loginBtn.click();
 				timeout();
+				test.pass("<p style=\"color:#1ccc0c; font-size:13px\"><b>Successfully Login.</b></p>");
+				String screenShotPath = GetScreenshot.capture(PageDriver.getCurrentDriver(), "LoginPass");
+				String dest = System.getProperty("user.dir") + "\\screenshots\\" + "LoginPass.png";
+				test.pass(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
 				}
 			
 		}
 		catch (Exception e)
 		{
-			System.out.println(e);
+			test.fail("<p style=\"color:#fa3c3c; font-size:13px\"><b>LoginButton is not locateable.Please check the error message.</b></p>");
+			Throwable t = new InterruptedException("Exception");
+			test.fail(t);
+			@SuppressWarnings("unused")
+			String screenShotPath = GetScreenshot.capture(PageDriver.getCurrentDriver(), "LoginButtonLocator");
+			String dest = System.getProperty("user.dir") + "\\screenshots\\" + "LoginButtonLocator.png";
+			test.fail(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
+			Assert.assertTrue(loginBtn.isDisplayed());
+			PageDriver.getCurrentDriver().quit();
 		}
 	}
 	
--- src/test/java/afsar/tests/LoginTest.java
+++ src/test/java/afsar/tests/LoginTest.java
@@ -1,26 +1,45 @@
 package afsar.tests;
 
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 
 import afsar.drivers.BaseDriver;
 import afsar.drivers.PageDriver;
 import afsar.pages.LoginPage;
+import afsar.utilities.ExtentFactory;
 
 public class LoginTest extends BaseDriver {
 	
 	// "URL" always keep in base driver as static variable;
 	//Open URL and maximize window----
+	
+	ExtentReports report;
+	ExtentTest parentTest;
+	ExtentTest childTest;
 	@BeforeClass
 	public void openUrl() {
 		
 		PageDriver.getCurrentDriver().manage().window().maximize();
 		PageDriver.getCurrentDriver().get(url);
+		report = ExtentFactory.getInstance();
+		parentTest = report.createTest("<p style=\"color:#FF6000; font-size:20px\"><b>ORANGE HRM</b></p>").assignAuthor("Nurul Afsar").assignDevice("Windows");
 	}
 	@Test(priority = 0)
-	public void loginTest() throws InterruptedException {
-		LoginPage loginpage = new LoginPage();
+	public void loginTest() throws InterruptedException, IOException {
+		childTest = parentTest.createNode("<p style=\"color:#05a16a; font-size:20px\"><b>LOGIN TEST</b></p>");
+		LoginPage loginpage = new LoginPage(childTest);
 		loginpage.login();
+	}
+	//------ For pushing report to the folder.
+	@AfterClass
+	public void afterClass() {
+		report.flush();
 	}
 
 }
